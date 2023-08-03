f0dfe227186331715cf1ef512e7724ce71c04778
salilthegreat <salilkumardas22@gmail.com>
2023-08-03 02:56:37 -0700

    Installation and routing done

---
+++ src/app/blog/layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BlogLayout = ({children}) => {
+  return (
+    <div>
+        <h2>This is BlogLayout</h2>
+        {children}</div>
+  )
+}
+
+export default BlogLayout
