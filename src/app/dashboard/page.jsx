476398c705f9ac9d4a5807bf327803e782b784dd
salilthegreat <salilkumardas22@gmail.com>
2023-08-12 11:40:20 -0700

    gogole auth setup and implementation done

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react'
 import styles from "./page.module.css"
 import useSWR from 'swr'
+import { useSession } from 'next-auth/react'
 
 
 const Dashboard = () => {
@@ -25,11 +26,13 @@ const Dashboard = () => {
   //   getData()
   // },[])
 
+  const session = useSession()
+
   const fetcher = (...args) => fetch(...args).then(res => res.json())
 
   const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
 
-  console.log(data)
+  console.log(session)
   return (
     <div className={styles.container}>Dashboard</div>
   )
