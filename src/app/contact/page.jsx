c96960e9dd93fc07a3e9c7073a04eccd1d74a8ce
salilthegreat <salilkumardas22@gmail.com>
2023-09-18 23:45:57 +0530

    Going live Effort 3

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -13,7 +13,7 @@ export const metadata = {
 const Contact = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.title}>Let's Keep in Touch</div>
+      <div className={styles.title}>Lets Keep in Touch</div>
       <div className={styles.content}>
         <div className={styles.imgContainer}>
           <Image src="https://img.freepik.com/free-vector/website-creator-concept-illustration_114360-3315.jpg?w=1060&t=st=1691673495~exp=1691674095~hmac=f096492ae4cb8fffabd971b6ad8bca5b9207470329ea863f071132158b759f8e" alt="" fill={true} className={styles.image} />
